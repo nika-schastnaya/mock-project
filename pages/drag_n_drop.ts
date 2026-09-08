142aa99e449bd512f5ff41a9fc88e99424092d81
LappyOrange <nikaschastnaya@gmail.com>
2026-09-08 17:09:33 +0200

    two additional tests to check drag-n-drop functionality

--- pages/drag_n_drop.ts
+++ pages/drag_n_drop.ts
@@ -4,10 +4,12 @@ import { Locator, Page } from "@playwright/test";
 export class DragAndDropPage extends BasePage {
   readonly blockA: Locator;
   readonly blockB: Locator;
+  readonly elements: Locator;
   constructor(protected page: Page) {
     super(page);
     this.blockA = page.locator("#column-a");
     this.blockB = page.locator("#column-b");
+    this.elements = page.locator('#columns > div');
   }
 
   async goto() {
--- tests/drag_and_drop.spec.ts
+++ tests/drag_and_drop.spec.ts
@@ -1,9 +1,35 @@
-import { test } from "@fixtures/page_fixture";
+import { test, expect } from "@fixtures/page_fixture";
 
 test.describe("drag and drop suite", () => {
-  test("object changes its order", async ({ dragAndDropPage }) => {
+  test("block changes its order if dropped over another block", async ({ dragAndDropPage }) => {
     await dragAndDropPage.goto();
 
     await dragAndDropPage.blockA.dragTo(dragAndDropPage.blockB);
+
+    await expect(dragAndDropPage.blockA).toContainText('B');
+    await expect(dragAndDropPage.blockB).toContainText('A');
+
+    await dragAndDropPage.blockA.dragTo(dragAndDropPage.blockB);
+
+    await expect(dragAndDropPage.blockA).toContainText('A');
+    await expect(dragAndDropPage.blockB).toContainText('B');
+  });
+
+  test('block does not change its order if dropped over self', async ({ dragAndDropPage }) => {
+    await dragAndDropPage.goto();
+
+    await dragAndDropPage.blockA.dragTo(dragAndDropPage.blockA);
+
+    await expect(dragAndDropPage.blockA).toContainText('A');
+    await expect(dragAndDropPage.blockB).toContainText('B');
+  });
+
+  test('block does not change its order if dropped over other element', async ({ page, dragAndDropPage }) => {
+    await dragAndDropPage.goto();
+
+    await dragAndDropPage.blockA.dragTo(page.getByRole('heading'));
+
+    await expect(dragAndDropPage.blockA).toContainText('A');
+    await expect(dragAndDropPage.blockB).toContainText('B');
   });
 });
