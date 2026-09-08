26ce0ae5bcc9f22e84320c4f17be7747109876c6
nika-schastnaya <nikaschastnaya@gmail.com>
2026-09-08 23:54:57 +0200

    upload tests

--- pages/upload_page.ts
+++ pages/upload_page.ts
@@ -1,16 +1,22 @@
 import { BasePage } from "@framework/pages/base_page";
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class UploadPage extends BasePage {
-    readonly chooseFileButton: Locator;
-    readonly uploadButton: Locator;
+  readonly chooseFileButton: Locator;
+  readonly uploadButton: Locator;
+  readonly uploadedFilesSection: Locator;
+  readonly dragDropUpload: Locator;
+  readonly fileInputs: Locator;
 
-    constructor (protected page: Page) {
-        super(page);
-        this.chooseFileButton = page.getByRole('button', { name: 'Choose file' });
-        this.uploadButton = page.getByRole('button', { name: 'Upload' });
-    }
-    async goto(){
-        await this.page.goto('/upload');
-    }
+  constructor(protected page: Page) {
+    super(page);
+    this.chooseFileButton = page.getByRole("button", { name: "Choose file" });
+    this.uploadButton = page.getByRole("button", { name: "Upload" });
+    this.uploadedFilesSection = page.locator("#uploaded-files");
+    this.dragDropUpload = page.locator("#drag-drop-upload");
+    this.fileInputs = page.locator('input[type="file"]');
+  }
+  async goto() {
+    await this.page.goto("/upload");
+  }
 }
--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -1,11 +1,75 @@
-import { test } from "@fixtures/page_fixture";
+import { test, expect } from "@fixtures/page_fixture";
+import { UploadPage } from "@pages/upload_page";
 
-test.describe('upload file suite', () => {
-    test('upload using button', async ({ uploadPage }) => {
-        await uploadPage.goto();
-        
-        await page.getByRole('button', { name: 'Choose File' }).setInputFiles('avocado_wink.png');
-        await page.getByRole('button', { name: 'Upload' }).click();
-        await expect(page.locator('#uploaded-files')).toContainText('avocado_wink.png');
-    });
+test.describe("upload file suite", () => {
+  test("upload using button", async ({ uploadPage }) => {
+    await uploadPage.goto();
+
+    await uploadPage.chooseFileButton.setInputFiles(
+      "../Downloads/avocado_wink.png",
+    );
+    await uploadPage.uploadButton.click();
+
+    await expect(uploadPage.uploadedFilesSection).toContainText(
+      "avocado_wink.png",
+    );
+  });
+
+  test.fail(
+    "upload using click on drag and drop zone",
+    async ({ uploadPage }) => {
+      await uploadPage.goto();
+
+      await uploadPage.dragDropUpload.click();
+      await uploadPage.fileInputs
+        .nth(1)
+        .setInputFiles("../Downloads/avocado_wink.png");
+
+      await expect(uploadPage.dragDropUpload).toContainText("avocado_wink.png");
+      await uploadPage.uploadButton.click();
+
+      await expect(uploadPage.uploadedFilesSection).toContainText(
+        "avocado_wink.png",
+      );
+    },
+  );
+
+  test.fail(
+    "upload empty file result in validation error",
+    async ({ uploadPage }) => {
+      await uploadPage.goto();
+
+      await uploadPage.uploadButton.click();
+
+      await expect(uploadPage.chooseFileButton).toHaveText("no file selected");
+    },
+  );
+
+  test.fail(
+    "multiple file upload via drag and drop area",
+    async ({ uploadPage }) => {
+      await uploadPage.goto();
+
+      await uploadPage.dragDropUpload.click();
+      await uploadPage.fileInputs
+        .nth(1)
+        .setInputFiles([
+          "../Downloads/avocado_wink.png",
+          "../Downloads/avocadotongue_q.png",
+        ]);
+
+      await expect(uploadPage.dragDropUpload).toContainText("avocado_wink.png");
+      await expect(uploadPage.dragDropUpload).toContainText(
+        "avocadotongue_q.png",
+      );
+      await uploadPage.uploadButton.click();
+
+      await expect(uploadPage.uploadedFilesSection).toContainText(
+        "avocado_wink.png",
+      );
+      await expect(uploadPage.uploadedFilesSection).toContainText(
+        "avocadotongue_q.png",
+      );
+    },
+  );
 });
